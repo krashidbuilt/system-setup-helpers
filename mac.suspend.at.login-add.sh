cbfeb7dcd9b973de7bed9dba16b6f4f531f58277
Ben Kauffman <ben@krashidbuilt.com>
2020-07-28 22:04:54 -0700

    damn copy paste

--- mac.suspend.at.login-add.sh
+++ mac.suspend.at.login-add.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-echo "This isn't currently supported: https://stackoverflow.com/questions/42637339/how-to-start-docker-for-mac-daemon-on-boot"
-exit 1
-
 if [ "$EUID" -ne 0 ]; then
     echo "Please run as root"
     exit
