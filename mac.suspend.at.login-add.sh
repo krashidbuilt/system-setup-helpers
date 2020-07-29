52b33ab7c1e47c1ae6bb44a7a0e423299424c511
Ben Kauffman <ben@krashidbuilt.com>
2020-07-28 22:13:55 -0700

    updated

--- mac.suspend.at.login-add.sh
+++ mac.suspend.at.login-add.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-if [ "$EUID" -ne 0 ]; then
-    echo "Please run as root"
+if [ "$EUID" -e 0 ]; then
+    echo "Please don't run as root"
     exit
 fi
 
 echo ""
 echo ""
 echo "Creating: com.krashidbuilt.user.suspend.onstartup.plist"
-\cp ./config/com.krashidbuilt.user.suspend.onstartup.plist /Library/LaunchAgents/com.krashidbuilt.user.suspend.onstartup.plist
-cat /Library/LaunchAgents/com.krashidbuilt.user.suspend.onstartup.plist
+\cp ./config/com.krashidbuilt.user.suspend.onstartup.plist ~/Library/LaunchAgents/com.krashidbuilt.user.suspend.onstartup.plist
+cat ~/Library/LaunchAgents/com.krashidbuilt.user.suspend.onstartup.plist
